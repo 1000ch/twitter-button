b10f251832cb25cf1e04f1d7629af7395a3d3aa8
1000ch <shogosensui@gmail.com>
2021-02-06 17:07:51 +0900

    test: add test

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { TwitterButton } from '../src/index';
+
+describe('TwitterButton', (): void => {
+  test('should be defined', (): void => {
+    expect(TwitterButton).toBeDefined();
+  });
+});
